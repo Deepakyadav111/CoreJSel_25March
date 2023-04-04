27dc5aac1c4e9c520e13f2fff8f685bf6fa6150f
Deepakyadav111 <dky31792@gmail.com>
2023-04-04 13:14:18 +0530

    Committing the Cunstructor codes

---
+++ Cunstructor/src/Package/Cunstructor.java
@@ -0,0 +1,28 @@
+package Package;
+
+public class Cunstructor {
+	
+	public Cunstructor()
+	
+	{
+	System.out.println("This Default Constructor");
+	}
+	
+	public Cunstructor(int a)
+	{	
+		System.out.println("This is one Prameterized Cunstrutor");
+	}
+	public Cunstructor(int a,int b)
+	{	
+		System.out.println("This is two Prameterized Cunstrutor");
+	}
+	public static void main(String[] args) {
+		
+		Cunstructor obc=new Cunstructor();
+		Cunstructor xyz=new Cunstructor(22);
+		Cunstructor abc=new Cunstructor(225,121);
+		
+
+	}
+
+}

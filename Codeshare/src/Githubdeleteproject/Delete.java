ee20683a6bc7d3ec1f026586e24fbb0a450d1cb5
Deepakyadav111 <dky31792@gmail.com>
2023-04-04 15:29:49 +0530

    want to share and or pull or delete

---
+++ Codeshare/src/Githubdeleteproject/Delete.java
@@ -0,0 +1,16 @@
+package Githubdeleteproject;
+
+public class Delete {
+
+	public void Deepak()
+	{
+		System.out.println("Deepak want to share ");
+	}
+
+	public static void main(String[] args) {
+		Delete abc=new Delete();
+		abc.Deepak();
+		
+	}
+
+}
